ed562da154d99366292ec8606b6daf5d34d59f19
Sacha FROMENT <sacha@hjkl.it>
2022-09-24 18:16:47 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+  import MinecraftButton from './lib/MinecraftButton.svelte';
+  let text = 'New trailer';
+  let size = 3;
+</script>
+
+<svelte:head>
+  <title>😢 Issue with the "new trailer" minecraft launcher button</title>
+</svelte:head>
+
+<header>
+  <h1>😢 Issue with the "new trailer" Minecraft launcher button</h1>
+</header>
+<main>
+  <p>
+    When I recently launched the Minecraft launcher, I was obviously curious
+    about the <em>brand new game</em> from
+    <a
+      href="https://upload.wikimedia.org/wikipedia/fr/0/01/Mojang_Studios_Logo_SVG.svg"
+    >
+      <img
+        class="mojang"
+        src="https://upload.wikimedia.org/wikipedia/fr/0/01/Mojang_Studios_Logo_SVG.svg"
+        alt="Mojang"
+      />
+    </a>
+    displayed on the sidebar :
+  </p>
+  <a
+    href="https://static.wikia.nocookie.net/logopedia/images/7/7d/Minecraft_Legends_logo.png"
+  >
+    <img
+      height="100px"
+      src="/Minecraft_Legends_logo.png"
+      alt="Logo minecraft legends"
+      style="height: 100px;"
+    />
+  </a>
+
+  <p>
+    Unfortunately, I also noticed a problem with their button/link to the game's
+    trailer (my pc is french) :
+  </p>
+  <video src="/fail.mp4" autoplay loop>
+    <track kind="captions" />
+  </video>
+  <p>
+    So I chose to reimplement the button using pure
+    <span class="cybernetic"> SVELTE (Cybernetically enhanced web apps)</span>
+    and CSS and 🎉TADA🎉 :
+  </p>
+
+  <form>
+    <div>
+      <label for="text"><b>Choose the text within the button</b></label>
+      <input id="text" name="text" type="text" bind:value={text} />
+    </div>
+    <div>
+      <label for="size"><b>Change the size of the element</b></label>
+      <input
+        type="range"
+        name="size"
+        id="size"
+        min="0.25"
+        step=".25"
+        max="10"
+        bind:value={size}
+      />
+    </div>
+  </form>
+  <div class="button">
+    <MinecraftButton {text} {size} />
+  </div>
+</main>
+
+<footer>
+  Me, the author, <a href="https://sacha.house">Sacha FROMENT</a> at your service
+  😎
+</footer>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Overpass&display=swap');
+  form {
+    margin: 1rem auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .mojang {
+    display: inline-block;
+    margin: 0 0.2rem;
+    height: 0.8rem;
+    justify-self: center;
+  }
+
+  .cybernetic {
+    font-family: 'Overpass', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+  }
+
+  .button {
+    margin: 2rem auto;
+    display: flex;
+    place-content: center;
+    /* background-color: rgba(175, 175, 175, 0.5); */
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    /* border: 1px solid rgba(70, 70, 70, 0.2); */
+  }
+
+  h1 {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  input {
+    min-width: fit-content;
+    padding: 0.5rem;
+  }
+  video {
+    margin: 1rem auto;
+    display: block;
+  }
+</style>
---
+++ src/lib/MinecraftButton.svelte
@@ -0,0 +1,170 @@
+<script lang="ts">
+  export let text = 'Minecraft';
+  export let size = 1;
+</script>
+
+<button style="--font-size: {size}rem">
+  <div class="border top" />
+  <div class="border bottom" />
+  <div class="border left" />
+  <div class="border right" />
+  <div class="border border-black-h" />
+  <div class="border border-black-w" />
+  <p class="text">
+    {text}
+  </p>
+</button>
+
+<style>
+  @import url('http://fonts.cdnfonts.com/css/minecraft-4');
+
+  :root {
+    --blue-shadow: #3fb9eb;
+    --blue-left: #3fb9eb;
+    --blue-right: #3fb9eb;
+    --blue-top: #acf2ff;
+    --blue-bottom: #3fb9eb;
+    --blue-main: #4ad5ff;
+    --blue-main-hover: #70dfff;
+    --text-color: #001236;
+
+    --shadow-color: rgba(255, 255, 255, 1);
+
+    --shadow-offset-max: 2em;
+
+    --font-size: 1rem;
+
+    --animate-rythm: cubic-bezier(0.75, 0.35, 0.25, 0.88);
+    --border-thickness: 0.2em;
+    --border-border-thickness: 0.1em;
+
+    --thickness: calc(var(--border-thickness) + var(--border-border-thickness));
+  }
+
+  button {
+    padding: 0.4rem 0.4rem;
+    margin: var(--thickness);
+    border-radius: 0;
+    border: none;
+    position: relative;
+    cursor: pointer;
+    font-size: var(--font-size);
+    background-color: var(--blue-main);
+  }
+
+  button:hover {
+    background-color: var(--blue-main-hover);
+  }
+
+  button::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    animation: shadow 1s alternate infinite var(--animate-rythm);
+    will-change: filter;
+    z-index: -4;
+    background-color: white;
+  }
+
+  @keyframes shadow {
+    from {
+      filter: none;
+    }
+    to {
+      filter: drop-shadow(0 0 var(--shadow-offset-max) var(--shadow-color));
+    }
+  }
+
+  .text {
+    margin: 0.3em 1.4em;
+    margin-bottom: 0.4em;
+    padding-bottom: em;
+    font-family: 'Minecraft', sans-serif;
+    animation: zimzoom 1s alternate infinite var(--animate-rythm);
+    text-shadow: 0 0.1em var(--blue-shadow);
+    color: var(--text-color);
+  }
+
+  .border {
+    position: absolute;
+    z-index: -2;
+  }
+
+  .border-black-h {
+    top: calc(var(--thickness) * -1);
+    left: calc(-1 * (var(--border-border-thickness)));
+    position: absolute;
+    height: calc(100% + var(--thickness) * 2);
+    width: calc(100% + var(--border-border-thickness) * 2);
+    background-color: black;
+  }
+
+  .border-black-w {
+    top: calc(-1 * (var(--border-border-thickness)));
+    left: calc(var(--thickness) * -1);
+    position: absolute;
+    height: calc(100% + var(--border-border-thickness) * 2);
+    width: calc(100% + var(--thickness) * 2);
+    background-color: black;
+  }
+
+  .top,
+  .bottom,
+  .left,
+  .right {
+    z-index: -1;
+  }
+
+  .top,
+  .bottom {
+    height: var(--border-thickness);
+    width: 100%;
+  }
+  .left,
+  .right {
+    height: 100%;
+    width: var(--border-thickness);
+  }
+
+  .top {
+    top: calc(var(--border-thickness) * -1);
+    left: 0;
+    width: 100%;
+    height: calc(1em + var(--border-thickness));
+    background-color: var(--blue-top);
+  }
+  .bottom {
+    bottom: calc(var(--border-thickness) * -1);
+    left: 0;
+    width: 100%;
+    height: calc(1em + var(--border-thickness));
+    background-color: var(--blue-bottom);
+  }
+  .left {
+    top: 0;
+    left: calc(var(--border-thickness) * -1);
+    width: calc(1em + var(--border-thickness));
+    height: 100%;
+    background-color: var(--blue-left);
+  }
+  .right {
+    top: 0;
+    right: calc(var(--border-thickness) * -1);
+    width: calc(1em + var(--border-thickness));
+    height: 100%;
+    background-color: var(--blue-right);
+  }
+
+  @keyframes zimzoom {
+    from {
+      transform: scale(1);
+    }
+
+    to {
+      transform: scale(1.2) translateY(-0.2rem);
+    }
+  }
+</style>
