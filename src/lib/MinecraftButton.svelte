6bc388b4bdeb290158a6704a5d55971484326f53
Sacha FROMENT <sacha@hjkl.it>
2022-09-25 13:24:14 +0200

    oh non le remix

--- src/lib/MinecraftButton.svelte
+++ src/lib/MinecraftButton.svelte
@@ -96,8 +96,6 @@
 
   .text {
     margin: 0.3em 1.4em;
-    padding-bottom: 0.2em;
-    line-height: 0.8em;
     font-family: 'Minecraft', sans-serif;
     animation: zimzoom 1s alternate infinite var(--animate-rythm);
     text-shadow: 0 0.1em var(--blue-shadow);
